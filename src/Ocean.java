814b9e6f70b3d0435b79eb8baa1216b4295f24c4
ankielbowicz <an.kielbowicz@gmail.com>
2020-05-24 11:59:51 +0200

    Architecture

--- src/Ocean.java
+++ src/Ocean.java
@@ -1,8 +1,23 @@
 import java.util.List;
-public class Ocean {
-    
-    private List<List<Square>> squares;
+
+public class Ocean{
+
+    private static final int WIDTH = 10
+    private static final int WEIGHT = 10
+
+    private List<List<Square>> Squares;
     private List<Ship> ships;
 
-    
-}
+
+    public Ocean(List<List<Square>> squares
+                List<Ship> ships) {
+            this.squares = squares;
+            this.ships = ships;        
+        }
+    @Override
+    public String toString( {
+        return"";
+    })
+
+
+    }
--- src/Ship.java
+++ src/Ship.java
@@ -1,6 +1,17 @@
+package battleship;
 import java.util.List;
 
 public class Ship {
-    private List <Square> squares;
     
+
+    private List<Square> squares;
+
+    public Ship(List<Square> squares){
+        this.squares = squares
+    }
+
+    @Override
+    public String toString(){
+        return "";
+    }
 }
