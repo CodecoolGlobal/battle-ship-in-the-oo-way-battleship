bea3259100c09e939ab3c6c3460c5eba438f73df
unknown <wloczykijtom@gmail.com>
2020-06-03 18:03:34 +0200

    Ship i Square

--- src/Ship.java
+++ src/Ship.java
@@ -1,18 +1,59 @@
-
+import java.util.Arrays;
 import java.util.List;
 
+
 public class Ship {
-    
+    Square[] shipPlace;
+    List<Square> shipNeighbor;
+    boolean sunk;
 
-    private List<Square> squares;
-
-    public Ship(List<Square> squares){
-        this.squares = squares;
+    public Ship(Square[] shipPlace){
+        this.sunk = false;
+        this.shipPlace = shipPlace;
+        for (int i = 0; i < shipPlace.length; i++){
+            shipPlace[i].makeShip();
+        }
+    }
+    public Square[] getPosition(){
+        return this.shipPlace;
     }
 
-
-    @Override
-    public String toString(){
-        return "";
+    public void addNeighbors(List<Square> neighbor){
+        this.shipNeighbor = neighbor;
+        for (int i = 0; i < this.shipNeighbor.size(); i++){
+            shipNeighbor.get(i).makeNeighbor();
+        }
     }
-}
+
+    public void updateState(){
+        this.sunk = true;
+        for (Square s: this.shipPlace){
+            if (s.isShip()){
+                this.sunk = false;
+            }
+        }
+        if (this.sunk){
+            for (int j = 0; j < this.shipPlace.length; j++){
+                this.shipPlace[j].makeSunk();
+            }
+            for (int i = 0; i < this.shipNeighbor.size(); i++){
+                this.shipNeighbor.get(i).makeMissFromNeighbor();
+            }
+    }}
+
+    public boolean isSunk(){
+        return this.sunk;
+    }
+
+    public boolean checkIfLocationIsPartoOfShip(Square location){
+        return Arrays.asList(this.shipPlace).contains(location);
+    }
+
+    public boolean tryToHitLocation(Square location){
+        boolean isHit = checkIfLocationIsPartoOfShip(location);
+        if (isHit){
+            location.makeHit();
+            this.updateState();
+        }
+        return isHit;
+    }
--- src/Square.java
+++ src/Square.java
@@ -1,15 +1,103 @@
 public class Square {
-
-    private final static String SYMBOL = "X";
-    private boolean isHit;
-
-
-    public void hit() {
-        isHit = true;
+    private String state;
+    private int x;
+    private int y;
+    public Square(int x, int y){
+        this.state = "ocean";
+        this.x = y;
+        this.y = x;
+    }
+    
+    
+    public void makeShip(){
+        if (this.state.equals("ocean")){
+            this.state = "ship";
+        }
     }
 
-    @Override
-    public String toString() {
-        return isHit ? SYMBOL : " ";
+    public int[] getXY(){
+        return new int[] {this.x, this.y};
+    }
+    
+    public void makeNeighbor(){
+        if (this.state.equals("ocean")){
+            this.state = "neighbor";
+        }
+    }
+    
+    public void makeHit(){
+        if (this.state.equals("ship")){
+            this.state = "hit";
+        } else if (this.state.equals("ocean") || this.state.equals("neighbor")) {
+            this.state = "miss";
+        }
+    }
+    
+    public void makeSunk(){
+        if (this.state.equals("hit")){
+            this.state = "sunk";
+        }
+    }
+    
+    public void makeMissFromNeighbor(){
+            this.state = "miss";
+    }
+    
+    public boolean isNeigbor(){
+        return this.state.equals("neighbor");
+    }
+    
+    public boolean isShip(){
+        return this.state.equals("ship");
+    }
+    public boolean isHit(){
+        return this.state.equals("hit");
+    }
+    
+    public boolean isSunk(){
+        return this.state.equals("sunk");
+    }
+    
+    public boolean isMiss(){
+        return this.state.equals("miss");
+    }
+
+    public boolean isOcean(){
+        return this.state.equals("ocean");
+    }
+
+    public String getState(){
+        return this.state;
+    }
+    
+    
+    public String showStatusToOwner() {
+        if (this.isShip()){
+            return "■";
+        } else if (this.isHit()){
+            return "×";
+        } else if (this.isSunk()){
+            return "#";
+        }else if (this.isMiss()){
+            return "Ø";
+        }
+        else {
+            return " ";
+         
+        }
+    }
+
+    public String showStatusToOponent() {
+        if (this.isSunk()){
+            return "#";
+        } else if (this.isHit()){
+            return "×";
+        }else if (this.isMiss()){
+            return "Ø";
+        }
+        else {
+            return " ";
+         
+        }
     }
 }
